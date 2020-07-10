4f12ae1e6312f1c211762ea5b16b04800567585c
Z-eddy <576172400@qq.com>
2020-07-11 00:07:17 +0800

    调用图片,python中无法用中文图片

---
+++ src/simpleConsole/simpleConsole.cpp
@@ -0,0 +1,11 @@
+﻿#include "opencv2/opencv.hpp"
+using namespace cv;
+
+int main()
+{
+	Mat src{ imread("./images/行走天涯柴犬.jpg") };
+	imshow("dogTitle", src);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
