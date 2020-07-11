1f099c7c598acd9ad3a459cfaf031f901630b205
Z-eddy <576172400@qq.com>
2020-07-11 22:39:36 +0800

    +、-、*、/

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,80 +1,33 @@
 ﻿#include<iostream>
 #include<string>
-#include<chrono>
-#include "opencv2/opencv.hpp"
-using std::chrono::high_resolution_clock;
-using std::string;
+#include<opencv2/opencv.hpp>
 using std::cout;
 using std::endl;
 using std::ends;
+using std::string;
 
 int main()
 {
-	string filePath{ "./images/dog.jpg" };
-	cv::Mat src{ cv::imread(filePath) };
-	if (src.empty()) {
-		cout << "file error" << endl;
-		return 0;
-	}
-	string winTitle{ "dogPic" };
-	cv::namedWindow(winTitle, cv::WINDOW_AUTOSIZE);
-	cv::imshow(winTitle, src);//显示原图
-	cv::waitKey(0);
-	auto nTime{ high_resolution_clock::now().time_since_epoch().count() };
-	//图片反色
-	auto r{ src.rows };//行数
-	auto c{ src.cols };//列数
-	auto ch{ src.channels() };//通道数
-	/*
-	//使用下标耗时54ms
-	for (int i{ 0 }; i != r; ++i) {
-		for (int j{ 0 }; j != c; ++j) {
-			if (ch == 3) {//如果是bgr颜色通道
-				auto &bgr{ src.at<cv::Vec3b>(i,j) };//获得坐标点的像素
-				for (int i{ 0 }; i != 3; ++i) {
-					bgr[i] = 255 - bgr[i];//反色
-				}
-			}
-			else if (ch == 1) {
-				auto &bgr{ src.at<uchar>(i,j) };
-				bgr = 255 - bgr;
-			}
-		}
-	}
-	*/
-	/*
-	//使用迭代器 213.5ms
-	if (ch == 3) {
-		for (auto beg{ src.begin<cv::Vec3b>() }; beg != src.end<cv::Vec3b>(); ++beg) {
-			for (int i{ 0 }; i != 3; ++i) {
-				(*beg)[i] = 255 - (*beg)[i];
-			}
-		}
-	}
-	else {
-		for (auto beg{ src.begin<uchar>() }; beg != src.end<uchar>(); ++beg) {
-			*beg = 255 - *beg;
-		}
-	}
-	*/
-	//使用指针 1.2ms//可怕
-	for (int i{ 0 }; i != r; ++i) {//对于每一行
-		auto rPtr{ src.ptr(i) };//获得行的指针
-		for (int j{ 0 }; j != c; ++j) {
-			if (ch == 3) {
-				rPtr[0] = 255 - rPtr[0];
-				rPtr[1] = 255 - rPtr[1];
-				rPtr[2] = 255 - rPtr[2];
-				//std::advance(rPtr, 3);
-				rPtr += 3;//手动挪动3个位置
-			}
-			else if (ch == 1) {
-				*rPtr = 255 - *rPtr;
-			}
-		}
-	}
-	auto eTime{ high_resolution_clock::now().time_since_epoch().count() };
-	cout << eTime - nTime << endl;
-	cv::imshow(winTitle, src);
+	string pathDir{ "F:/Code/openCV/opencv/sources/samples/data/" };
+	string img0{ pathDir + "WindowsLogo.jpg" };
+	string img1{ pathDir + "LinuxLogo.jpg" };
+	cv::Mat src0{ cv::imread(img0) };
+	cv::imshow("img0", src0);
+	cv::Mat src1{ cv::imread(img1) };
+	cv::imshow("img1",src1);
+
+	cv::Mat result{ cv::Mat::zeros(src0.size(),src0.type()) };
+	cv::add(src0, src1, result);
+	cv::imshow("add", result);
+
+	cv::subtract(src0, src1, result);
+	cv::imshow("subtract",result);
+
+	cv::multiply(src0, src1, result);
+	cv::imshow("multiply", result);
+
+	cv::divide(src0, src1, result);
+	cv::imshow("divide",result);
+
 	cv::waitKey(0);
 }
