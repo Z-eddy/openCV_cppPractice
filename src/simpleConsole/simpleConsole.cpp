1c083452f13765e6dab5bb7a7a0111d22ebf0d60
Z-eddy <zrendless@foxmail.com>
2020-07-11 10:26:52 +0800

    imshow不能做交互,放入namedWindow后可以

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,11 +1,12 @@
-﻿#include "opencv2/opencv.hpp"
-using namespace cv;
-
+﻿#include<iostream>
+#include "opencv2/opencv.hpp"
+using std::cout;
+using std::endl;
+using std::ends;
 int main()
 {
-	Mat src{ imread("./images/行走天涯柴犬.jpg") };
-	imshow("dogTitle", src);
-	waitKey(0);
-	destroyAllWindows();
-	return 0;
+	cv::Mat src{ cv::imread("./images/行走天涯柴犬.jpg") };
+	cv::namedWindow("pic", cv::WINDOW_AUTOSIZE);
+	cv::imshow("pic", src);
+	cv::waitKey(0);
 }
