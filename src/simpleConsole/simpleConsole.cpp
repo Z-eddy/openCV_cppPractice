cfab5a29eb22399fddd3075ec5fecf5a2931d3f1
Z-eddy <576172400@qq.com>
2020-07-18 00:00:47 +0800

    resize的4种插值方法,接收Size则忽略后面两个x\y缩放比
    Size为0时看后面的缩放比

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,30 +1,33 @@
-﻿#include<string>
-#include<iostream>
+﻿#include<iostream>
+#include<string>
 #include "opencv2/opencv.hpp"
-using std::string;
 using std::endl;
 using std::ends;
 using std::cout;
+using std::string;
 
 int main()
 {
-	auto src{ cv::imread("./images/dog.jpg") };
+	auto src{ cv::imread("images/dog.jpg") };
 	if (src.empty()) {
-		cout << "error" << endl;
-		return 0;
+		cout << "can't open file" << endl;
 	}
 	cv::imshow("original", src);
 
 	cv::Mat dst;
+	cv::Size size(src.rows * 0.5, src.cols * 0.5);
 
-	cv::flip(src, dst, 0);
-	cv::imshow("x_flip", dst);
+	cv::resize(src, dst, { 0,0 }, 2, 2, cv::INTER_NEAREST);//Size为0时看后面的缩放比
+	cv::imshow("NEARST", dst);
 
-	cv::flip(src, dst, 1);
-	cv::imshow("y_flip", dst);
+	cv::resize(src, dst, size, 0, 0, cv::INTER_LINEAR);
+	cv::imshow("LINEAR", dst);
 
-	cv::flip(src, dst, -1);
-	cv::imshow("xy_flip", dst);
+	cv::resize(src, dst, size, 0, 0, cv::INTER_CUBIC);
+	cv::imshow("CUBIC", dst);
+
+	cv::resize(src, dst, size, 0, 0, cv::INTER_LANCZOS4);
+	cv::imshow("LANCZONS", dst);
 
 	cv::waitKey(0);
 }
