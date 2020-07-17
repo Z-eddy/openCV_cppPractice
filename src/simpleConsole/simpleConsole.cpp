6d615f29e63e9ad8e86345058180cb52fa73475c
Z-eddy <zrendless@foxmail.com>
2020-07-17 16:53:39 +0800

    VideoCapture简单的摄像头读取

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,47 +1,27 @@
 ﻿#include<iostream>
 #include<string>
-#include<vector>
+#include<chrono>
+#include<thread>
 #include "opencv2/opencv.hpp"
-using std::vector;
-using std::string;
 using std::cout;
 using std::endl;
 using std::ends;
+using std::string;
 
 int main()
 {
-	auto src{ cv::imread("./images/dog.jpg") };
-	string winTitle{ "imgShow" };
+	cv::VideoCapture v{ 0 };//使用摄像头
+	if (!v.isOpened()) {
+		cout << "摄像头无法打开" << endl;
+	}
+	cv::Mat frame;
+	string winTitle{ "cameraDemo" };
 	cv::namedWindow(winTitle, cv::WINDOW_AUTOSIZE);
-	//原图显示
-	cv::imshow(winTitle, src);
-	cv::waitKey(0);
-
-	//灰度图转换
-	cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);//此时只有1个通道
-	src.convertTo(src, CV_32F);
-
-	cv::Mat dst{ cv::Mat::zeros(src.size(),CV_32FC1) };
-	//MINMAX归一化
-	cv::normalize(src, dst, 1, 0, cv::NORM_MINMAX);
-	dst *= 255;//此数值是估计的,过大则曝光强烈,否则黯淡
-	dst.convertTo(dst, CV_8UC1);
-	cv::imshow("MINMAX", dst);
-	//INF归一化
-	cv::normalize(src, dst, 1, 0, cv::NORM_INF);
-	dst *= 255;
-	dst.convertTo(dst, CV_8UC1);
-	cv::imshow("INF", dst);
-	//L1归一化
-	cv::normalize(src, dst, 1, 0, cv::NORM_L1);
-	dst *= 10000000;
-	dst.convertTo(dst, CV_8UC1);
-	cv::imshow("L1", dst);
-	//L2归一化
-	cv::normalize(src, dst, 1, 0, cv::NORM_L2);
-	dst *= 10000;
-	dst.convertTo(dst, CV_8UC1);
-	cv::imshow("L2", dst);
-
-	cv::waitKey(0);
+	while (v.read(frame)) {
+		cv::imshow(winTitle, frame);
+		auto keyVal{ cv::waitKey(16) };
+		if (keyVal == 27) {//退出键则退出
+			break;
+		}
+	}
 }
