73c08bdaa2a85e33af5ad55dcd3e89b1f42bbca3
Z-eddy <576172400@qq.com>
2020-07-20 00:01:58 +0800

    掩膜与bitwise的结合使用

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -11,17 +11,13 @@ using std::vector;
 int main()
 {
 	cv::Mat src{ cv::imread("G:/Practice/openCV/images/TinyGreen.PNG") };
-	cv::Mat dst;
-	cv::cvtColor(src, dst, cv::COLOR_BGR2HSV);//先转换hsv图
-	cv::Mat mask;
-	cv::inRange(dst, cv::Scalar{ 36,43,46 }, cv::Scalar{ 99,255,255 }, mask);//获得mssk掩膜
-	cv::bitwise_not(mask, mask);//反转mask,人白背景黑,人255,背景0
-	cv::imshow("mask", mask);//显示mask
-	cv::Mat person;
-	cv::Mat temp{ cv::Mat::zeros(src.size(),src.type()) };
-	temp.setTo(cv::Scalar{ 255,255,255 });
-	cv::bitwise_and(src,temp, person, mask);//按照mask有效区内,src与temp做and计算
-	cv::imshow("result", person);
-
+	cv::Mat hsv;
+	cv::cvtColor(src, hsv, cv::COLOR_BGR2HSV);//先转换hsv图
+	cv::Mat origiMask;//掩膜
+	cv::inRange(hsv, cv::Scalar{ 36,43,46 }, cv::Scalar{ 99,255,255 }, origiMask);//获得掩膜,人黑背景白
+	
+	cv::Mat blue{ cv::Mat{cv::Mat::zeros(src.size(),src.type())}.setTo(cv::Scalar{255,0,0}) };//蓝色背景
+	cv::bitwise_and(blue, blue,src, origiMask);//origiMask的有效区(背景区)内,blue与blue做and计算后,替换src中对应的像素
+	cv::imshow("bluePerson",src);
 	cv::waitKey(0);
 }
