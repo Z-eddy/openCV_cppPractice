40e7be143c9aaa3d3ea28c03c89a89d9a7422856
Zr <zrendless@foxmail.com>
2020-07-22 16:21:46 +0800

    blur图像卷积模糊

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,34 +1,16 @@
-﻿#include<iostream>
-#include<string>
-#include<vector>
+﻿#include<string>
 #include "opencv2/opencv.hpp"
-using std::cout;
-using std::endl;
-using std::ends;
 using std::string;
-using std::vector;
 
 int main()
 {
-	cv::Mat image{ cv::imread("E:/practice/openCV/material/images/target.png") };
-	cv::imshow("original",image);
-	cv::Mat theModule{ cv::imread("E:/practice/openCV/material/images/sample.png") };
-	cv::imshow("module",theModule);
-	cv::cvtColor(image, image, cv::COLOR_BGR2HSV);
-	cv::cvtColor(theModule, theModule, cv::COLOR_BGR2HSV);
+	auto src{ cv::imread("images/dog.jpg") };
+	string winTitle{ "demo" };
+	cv::namedWindow(winTitle,cv::WINDOW_AUTOSIZE);
+	cv::imshow(winTitle, src);
+	cv::waitKey(0);
 
-	//计算直方图
-	vector<cv::Mat>mVe{ theModule };
-	cv::Mat roiHist;
-	cv::calcHist(mVe, { 0,1 }, cv::Mat{}, roiHist, { 30,32 }, { 0,180,0,256 });
-	cv::normalize(roiHist, roiHist, 255, 0, cv::NORM_MINMAX);
-	//cv::resize(roiHist, roiHist, { 400,400 });
-
-	//计算图像直方图反向投影
-	vector<cv::Mat>imaVe{ image };
-	cv::Mat dst;
-	cv::calcBackProject(imaVe, { 0,1 }, roiHist, dst, { 0,180,0,255 }, 1);
-	cv::imshow("result", dst);
-
+	cv::blur(src, src, { 3,3 });//3*3卷积核/掩膜
+	cv::imshow("卷积模糊后图像", src);
 	cv::waitKey(0);
 }
