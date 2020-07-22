5b4f38311a1fa5f68b7bb829f0ed4792c4befe17
Zr <576172400@qq.com>
2020-07-22 23:47:42 +0800

    GaussianBlur高斯卷积模糊

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -8,9 +8,8 @@ int main()
 	string winTitle{ "demo" };
 	cv::namedWindow(winTitle,cv::WINDOW_AUTOSIZE);
 	cv::imshow(winTitle, src);
-	cv::waitKey(0);
 
-	cv::blur(src, src, { 3,3 });//3*3卷积核/掩膜
-	cv::imshow("卷积模糊后图像", src);
+	cv::GaussianBlur(src, src, { 3,3 }, 5);
+	cv::imshow("卷积模糊后图像", src);//高斯卷积模糊
 	cv::waitKey(0);
 }
