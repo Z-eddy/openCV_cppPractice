4ce8d83c7c1b7ae71abf00d5b5f0633f2b5a6df8
Zr <576172400@qq.com>
2020-08-01 23:49:37 +0800

    Laplacian梯度算子,二阶求导算法

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,38 +1,12 @@
-﻿#include"opencv2/opencv.hpp"
-
+﻿#include "opencv2/opencv.hpp"
 int main()
 {
 	auto src{ cv::imread("images/dog.jpg").getUMat(cv::ACCESS_RW) };
-	cv::UMat dstTemp, dst;
-
-	//robert x方向算子??y??
-	cv::Mat robert_x{ {2,2},{1,0,0,-1} };
-	cv::filter2D(src, dstTemp, CV_16S, robert_x);
-	cv::convertScaleAbs(dstTemp, dst);
-	cv::imshow("robert_x", dst);
-	//robert y方向算子??x??
-	cv::Mat robert_y{ {2,2},{0,-1,1,0} };
-	cv::filter2D(src, dstTemp, CV_16S, robert_y);
-	cv::convertScaleAbs(dstTemp, dst);
-	cv::imshow("robert_y", dst);
-	//prewitt x方向算子
-	cv::Mat prewitt_x{ {3,3},
-		{ -1,0,1,
-		-1,0,1,
-		-1,0,1 }
-	};
-	cv::filter2D(src, dstTemp, CV_32F, prewitt_x);
-	cv::convertScaleAbs(dstTemp, dst);
-	cv::imshow("prewittX", dst);
-	//prewitt y方向算子
-	cv::Mat prewitt_y{ {3,3},
-		{-1,-1,-1,
-		0,0,0,
-		1,1,1 }
-	};
-	cv::filter2D(src, dstTemp, CV_32F, prewitt_y);
-	cv::convertScaleAbs(dstTemp, dst);
-	cv::imshow("prewittY", dst);
+	cv::UMat tempDst, dst;
+	//ksize为1指4领域,必须为奇数,如果为其他非1奇数则是8邻域
+	cv::Laplacian(src, tempDst, CV_32F, 1, 1, 150);
+	cv::convertScaleAbs(tempDst, dst);
+	cv::imshow("Laplacian", dst);
 
 	cv::waitKey(0);
 }
