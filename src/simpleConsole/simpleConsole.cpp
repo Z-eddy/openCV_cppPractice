f8c0f5e22773cd5fee723c377eab338d67a7d167
Zr <576172400@qq.com>
2020-08-02 00:11:23 +0800

    加入高斯卷积的拉普拉斯算子

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -2,9 +2,15 @@
 int main()
 {
 	auto src{ cv::imread("images/dog.jpg").getUMat(cv::ACCESS_RW) };
-	cv::UMat tempDst, dst;
+	cv::UMat tempGauss, tempDst, dst;
 	//ksize为1指4领域,必须为奇数,如果为其他非1奇数则是8邻域
-	cv::Laplacian(src, tempDst, CV_32F, 1, 1, 150);
+	if (true) {
+		cv::GaussianBlur(src, tempGauss, { 3,3 }, 0);
+		cv::Laplacian(tempGauss, tempDst, CV_32F, 1, 1, 150);
+	}
+	else {
+		cv::Laplacian(src, tempDst, CV_32F, 1, 1, 150);
+	}
 	cv::convertScaleAbs(tempDst, dst);
 	cv::imshow("Laplacian", dst);
 
