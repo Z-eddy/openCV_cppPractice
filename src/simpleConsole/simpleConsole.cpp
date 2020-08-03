3a6cc0450a6c24ad87c0a54fb376811b7bb1f93a
赵睿 <zrendless@foxmail.com>
2020-08-04 00:08:07 +0800

    pyrDown,pyrUp高斯金字塔reduce,expand

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -3,11 +3,15 @@
 int main()
 {
 	auto src{ cv::imread("images/dog.jpg").getUMat(cv::ACCESS_RW) };
-	cv::UMat tempDst,dst;
-	cv::Canny(src,tempDst, 80, 240, 3, false);//默认的使用sobel算子计算x/y方向的inputArray,非默认的自己指定xInput,yInput
-	cv::cvtColor(tempDst, dst, cv::COLOR_GRAY2BGR);
-	cv::putText(dst, "Canny", { 20,20 }, cv::FONT_ITALIC, 1.0, { 0,0,255 }, 2);//但是不能打印中文?
-	cv::imshow("Canny边缘检测器", dst);
+	cv::imshow("original", src);
+	
+	cv::UMat pyrDownDst;
+	cv::pyrDown(src, pyrDownDst);//高斯金字塔reduce
+	cv::imshow("pyrDownDst", pyrDownDst);
+
+	cv::UMat pyrUpDst;
+	cv::pyrUp(pyrDownDst, pyrUpDst);//高斯金字塔expand
+	cv::imshow("pyrUpDst", pyrUpDst);
 
 	cv::waitKey(0);
 }
