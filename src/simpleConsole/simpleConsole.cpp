f725fc2897f105855ac5d7ca37c2948639ffc5eb
赵睿 <zrendless@foxmail.com>
2020-08-03 23:57:57 +0800

    putText打印字符

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -3,8 +3,10 @@
 int main()
 {
 	auto src{ cv::imread("images/dog.jpg").getUMat(cv::ACCESS_RW) };
-	cv::UMat dst;
-	cv::Canny(src, dst, 80, 240, 3, false);//默认的使用sobel算子计算x/y方向的inputArray,非默认的自己指定xInput,yInput
+	cv::UMat tempDst,dst;
+	cv::Canny(src,tempDst, 80, 240, 3, false);//默认的使用sobel算子计算x/y方向的inputArray,非默认的自己指定xInput,yInput
+	cv::cvtColor(tempDst, dst, cv::COLOR_GRAY2BGR);
+	cv::putText(dst, "Canny", { 20,20 }, cv::FONT_ITALIC, 1.0, { 0,0,255 }, 2);//但是不能打印中文?
 	cv::imshow("Canny边缘检测器", dst);
 
 	cv::waitKey(0);
