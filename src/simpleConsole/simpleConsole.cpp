afbce47efe9a0852e6422f9eeb81c030b605201d
赵睿 <zrendless@foxmail.com>
2020-08-06 01:43:40 +0800

    matchTemplate框选出匹配结果

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -2,16 +2,23 @@
 
 int main()
 {
-	auto src{ cv::imread("images/dog.jpg") };
-	cv::imshow("origianl", src);
-	cv::Mat temp0, temp1, dst;
-	cv::pyrDown(src, temp0);//先reduce
-	cv::pyrUp(temp0, temp1, src.size());//然后expand
-	cv::subtract(src, temp1, temp0);//计算拉普拉斯金字塔
-	cv::imshow("laplacian pyramid", temp0);
-	cv::imshow("expand", temp1);
-	cv::add(temp1, temp0, dst);//再叠加expand与Laplacian
-	cv::imshow("reverse to original", dst);//复原图
+	auto src{ cv::imread("E:/practice/openCV/material/images/llk.jpg") };
+	auto tpl{ cv::imread("E:/practice/openCV/material/images/llk_tpl.png") };
+	cv::imshow("original", src);
+	cv::Mat dst;
+	cv::matchTemplate(src, tpl, dst, cv::TM_CCORR_NORMED);
+	cv::imshow("dst", dst);
+
+	auto h{ dst.rows }, w{ dst.cols };//结果的行列
+	for (int i{ 0 }; i != h; ++i) {
+		for (int j{ 0 }; j != w; ++j) {
+			auto v{ dst.at<float>(i,j) };
+			if (v > 0.95) {
+				cv::rectangle(src, cv::Point{ j,i }, cv::Point{ j + tpl.cols,i + tpl.rows}, cv::Scalar{ 0,0,255 }, 1, 8, 0);//框选出匹配图
+			}
+		}
+	}
+	cv::imshow("rectangle",src);
 
 	cv::waitKey(0);
 }
