4e7eb7d87020fcad847a1bf25b52923d064e6f43
赵睿 <zrendless@foxmail.com>
2020-08-06 15:39:50 +0800

    通过遍历分割图像为二值图

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -2,24 +2,23 @@
 
 int main()
 {
-	auto src{ cv::imread("E:/practice/openCV/material/images/llk.jpg") };
-	auto tpl{ cv::imread("E:/practice/openCV/material/images/llk_tpl.png") };
+	auto src{ cv::imread("images/dog.jpg") };
 	cv::imshow("original", src);
-	cv::Mat dst;
-	cv::matchTemplate(src, tpl, dst, cv::TM_CCORR_NORMED);
-	cv::imshow("dst", dst);
-
-	auto row{ dst.rows }, col{ dst.cols };//结果的行列
+	cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+	auto row{ src.rows }, col{ src.cols };
+	unsigned char val{ 127 };
 	for (int i{ 0 }; i != row; ++i) {
-		auto p{ dst.ptr<float>(i) };
-		for (int j{ 0 }; j != col; ++j) {
-			//auto v{ dst.at<float>(i,j) };//使用指针效率更高
-			if (*p++ > 0.95) {
-				cv::rectangle(src, cv::Point{ j,i }, cv::Point{ j + tpl.cols,i + tpl.rows}, cv::Scalar{ 0,0,255 }, 1, 8, 0);//框选出匹配图
+		auto p{ src.ptr(i) };
+		for (int j{ 0 }; j != col; ++p, ++j) {
+			if (*p > val) {
+				*p = 255;
+			}
+			else {
+				*p = 0;
 			}
 		}
 	}
-	cv::imshow("rectangle",src);
+	cv::imshow("dst", src);
 
 	cv::waitKey(0);
 }
