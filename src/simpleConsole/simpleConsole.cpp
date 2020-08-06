d9290e158dd449cea9af258597d84b6688bd978f
赵睿 <zrendless@foxmail.com>
2020-08-07 02:01:36 +0800

    threshold阈值

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -2,24 +2,25 @@
 
 int main()
 {
-	auto src{ cv::imread("images/dog.jpg") };
-	cv::imshow("original", src);
+	auto src{ cv::imread("C:/Users/Zr/Desktop/temp/test.png") };
 	cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
-	auto row{ src.rows }, col{ src.cols };
-	//unsigned char val{ 127 };
-	unsigned char val= cv::mean(src)[0];
-	for (int i{ 0 }; i != row; ++i) {
-		auto p{ src.ptr(i) };
-		for (int j{ 0 }; j != col; ++p, ++j) {
-			if (*p > val) {
-				*p = 255;
-			}
-			else {
-				*p = 0;
-			}
-		}
-	}
-	cv::imshow("dst", src);
+
+	cv::Mat dst;
+
+	cv::threshold(src, dst, 127, 255, cv::THRESH_BINARY);//按照127的阈值进行分割,大于为1，小于为0
+	cv::imshow("THRESH_BINARY", dst);
+
+	cv::threshold(src, dst, 127, 255, cv::THRESH_BINARY_INV);//按照127的阈值进行分割,大于为0，小于为1
+	cv::imshow("THRESH_BINARY_INV", dst);
+
+	cv::threshold(src, dst, 127, 255, cv::THRESH_TRUNC);//大于127的都成为127
+	cv::imshow("THRESH_TRUNC", dst);
+
+	cv::threshold(src, dst, 127, 255, cv::THRESH_TOZERO);//小于127的都为0
+	cv::imshow("THRESH_TOZERO", dst);
+
+	cv::threshold(src, dst, 127, 255, cv::THRESH_TOZERO_INV);//大于127的都为0
+	cv::imshow("THRESH_TOZERO_INV", dst);
 
 	cv::waitKey(0);
 }
