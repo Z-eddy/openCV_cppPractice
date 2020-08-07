186460dd373a23e6ced93b6227b10b17ca949f68
赵睿 <zrendless@foxmail.com>
2020-08-08 00:38:20 +0800

    thresh_triangle二值化图像,triangle自动阈值

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,16 +1,19 @@
 ﻿#include<iostream>
 #include "opencv2/opencv.hpp"
-using std::cout;
 using std::endl;
 using std::ends;
+using std::cout;
+
 int main()
 {
 	auto src{ cv::imread("images/dog.jpg") };
-	cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+	cv::imshow("original", src);
+	cv::Mat tempSrc;
+	cv::cvtColor(src, tempSrc, cv::COLOR_BGR2GRAY);//转化为灰度图好进行二值化
 	cv::Mat dst;
-	auto v{ cv::threshold(src, dst, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU) };
-	cout << v << endl;
-	cv::imshow("dst", dst);
+	auto val{ cv::threshold(tempSrc, dst, 0, 255, cv::THRESH_BINARY | cv::THRESH_TRIANGLE) };
+	cout << val << endl;
+	cv::imshow("threshold_triangle", dst);
 
 	cv::waitKey(0);
 }
