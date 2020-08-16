add769b619d14c9c0cc323fe56e6559ae483f5d7
赵睿 <zrendless@foxmail.com>
2020-08-16 23:30:43 +0800

    minAreaRect最小矩形轮廓发现,boundingRect最大外接矩形发现
    先使用Canny发现轮廓,然后使用dilate膨胀连接边缘线,再使用findContours对膨胀的结果统计轮廓,最后对轮廓进行矩形绘制

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,24 +1,31 @@
-﻿#include "opencv2/opencv.hpp"
-#include<vector>
-#include<iostream>
+﻿#include<vector>
+#include "opencv2/opencv.hpp"
 using std::vector;
-using std::cout;
-using std::endl;
-using std::ends;
 
 int main()
 {
-	auto src{ cv::imread("images/dog.jpg") };
-	cv::imshow("original", src);
+	auto src{ cv::imread("E:/practice/openCV/material/images/stuff.jpg") };
+	cv::imshow("src", src);
+	cv::Mat canny;
+	cv::Canny(src, canny,80,160);
 
-	cv::Mat tempCanny;
-	cv::Canny(src, tempCanny, 100, 200);//使用canny默认的sobel算法进行轮廓发现
+	auto k{ cv::getStructuringElement(cv::MORPH_RECT,{3,3}) };
+	cv::Mat theDialate;
+	cv::dilate(canny, theDialate, k);//膨胀,连接断开的边缘
 
-	vector<vector<cv::Point>> contours;//轮廓点集合
-	cv::findContours(tempCanny, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-
+	vector<vector<cv::Point>>contours;
+	cv::findContours(theDialate, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 	for (int i{ 0 }; i != contours.size(); ++i) {
-		cv::drawContours(src, contours, i, cv::Scalar{ 0,0,255 });
+		//最小轮廓矩形发现
+		auto routateRect{ cv::minAreaRect(contours.at(i)) };
+		cv::Point2f fourLine[4];
+		routateRect.points(fourLine);
+		for (int i{ 0 }; i != 4; ++i) {
+			cv::line(src, fourLine[i % 4], fourLine[(i + 1) % 4], cv::Scalar{ 0,0,255 });
+		}
+		//最大外接矩形发现
+		auto rect{ cv::boundingRect(contours.at(i)) };
+		cv::rectangle(src,rect, cv::Scalar{ 0,0,255 });
 	}
 	cv::imshow("dst", src);
 
