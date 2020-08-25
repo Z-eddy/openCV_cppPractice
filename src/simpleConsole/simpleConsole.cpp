1735bd2391e07536c972e74b2cc871c80f076797
赵睿 <zrendless@foxmail.com>
2020-08-25 22:49:38 +0800

    moment计算矩,使用矩计算出轮廓自身的中心

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,43 +1,55 @@
 ﻿#include<vector>
 #include<iostream>
-#include "opencv2/opencv.hpp"
-using std::endl;
-using std::ends;
+#include"opencv2/opencv.hpp"
 using std::cout;
+using std::ends;
+using std::endl;
 using std::vector;
+using std::min;
+using std::max;
 
 int main()
 {
-	auto src{ cv::imread("E:/practice/openCV/material/images/contours.png") };
-	cv::Mat canny;
-	cv::Canny(src, canny, 80, 160);//轮廓勾勒
-	vector<vector<cv::Point>> contours;
-	cv::findContours(canny, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	auto src{ cv::imread("E:/practice/openCV/material/images/stuff.jpg").getUMat(cv::ACCESS_RW) };
+	cv::imshow("original", src);
+	cv::UMat canny;
+	cv::Canny(src, canny, 80, 160);
+	cv::UMat k{ cv::getStructuringElement(cv::MORPH_RECT,{3,3}).getUMat(cv::ACCESS_RW) };
+	cv::UMat dilate;
+	cv::dilate(canny, dilate, k);
+	vector<cv::UMat> contours;
+	cv::findContours(dilate.getMat(cv::ACCESS_RW), contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 	for (auto item : contours) {
-		auto rotatedRect{ cv::minAreaRect(item) };
-		auto center{ rotatedRect.center };//获得圆心
-		vector<cv::Point2f>curve;
-		cv::approxPolyDP(item, curve, 4, true);
-		auto size{ curve.size() };//每条曲线的点数
-		switch (size)
-		{
-		case 3:
-			cv::putText(src, "Triangle", center, cv::FONT_ITALIC, 0.8, { 0,0,255 });
-			break;
-		case 4:
-			cv::putText(src, "Rectangle", center, cv::FONT_ITALIC, 0.8, { 0,0,255 });
-			break;
-		case 6:
-			cv::putText(src, "Polygon", center, cv::FONT_ITALIC, 0.8, { 0,0,255 });
-			break;
-		default:
-			break;
+		//获得轮廓的中心
+		auto moment{ cv::moments(item) };
+		int cx = moment.m10 / moment.m00;
+		int cy = moment.m01 / moment.m00;
+		//获得外接矩形的长宽比
+		auto rotateRect{ cv::minAreaRect(item) };
+		vector<cv::Point2f> lines(4);
+		rotateRect.points(&lines[0]);
+		auto w{ rotateRect.size.width };
+		auto h{ rotateRect.size.height };
+		auto ratio{ min(w, h) / max(w,h) };
+		if (ratio > 0.9) {//趋向于正方形
+			cv::circle(src, { cx,cy }, 2, { 255,0,0 }, -1);
+			for (auto beg{ lines.cbegin() }; beg != lines.cend(); ++beg) {//绘制矩形框
+				auto second{ beg + 1 };
+				if (beg == lines.cend() - 1) {//如果当前点是最后一点,则连接到第一点
+					second = lines.cbegin();
+				}
+				cv::line(src, *beg, *second, { 0,0,255 }, 2);
+			}
 		}
-		if (size < 3 || size > 6 && size < 10) {
-			cv::putText(src, "Unknown", center, cv::FONT_ITALIC, 0.8, { 0,0,255 });
-		}
-		else if (size >= 10) {
-			cv::putText(src, "Cycle", center, cv::FONT_ITALIC, 0.8, { 0,0,255 });
+		else if (ratio < 0.5) {//长方形
+			cv::circle(src, { cx,cy }, 2, { 0,0,255 }, -1);
+			for (auto beg{ lines.cbegin() }; beg != lines.cend(); ++beg) {//绘制矩形框
+				auto second{ beg + 1 };
+				if (beg == lines.cend() - 1) {//如果当前点是最后一点,则连接到第一点
+					second = lines.cbegin();
+				}
+				cv::line(src, *beg, *second, { 255,0,255 }, 2);
+			}
 		}
 	}
 	cv::imshow("dst", src);
