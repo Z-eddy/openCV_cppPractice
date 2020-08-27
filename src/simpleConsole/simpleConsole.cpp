3f5b2eb75162d512406aca0d7bcb89b5507e87f2
赵睿 <zrendless@foxmail.com>
2020-08-27 23:52:17 +0800

    fitEllipse轮廓圆拟合

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,46 +1,23 @@
 ﻿#include<vector>
-#include<iostream>
-#include "opencv2/opencv.hpp"
+#include"opencv2/opencv.hpp"
 using std::vector;
-using std::cout;
-using std::ends;
-using std::endl;
 
-vector<cv::UMat> getContours(cv::UMat arg) {//从原图读取,然后进行轮廓提取
-	if (arg.empty()) {//空的就返回
-		cout << "empty" << endl;
-		return{};
-	}
-	cv::UMat gray,threshold;
-	cv::cvtColor(arg, gray, cv::COLOR_BGR2GRAY);
-	cv::threshold(gray, threshold, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-	vector<cv::UMat> contours;
-	cv::findContours(threshold, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-	return contours;
-}
 int main()
 {
-	auto src0{ cv::imread("E:/practice/openCV/material/images/abc.png").getUMat(cv::ACCESS_FAST) };
-	auto src1{ cv::imread("E:/practice/openCV/material/images/a5.png").getUMat(cv::ACCESS_FAST) };
-	auto contours0{ getContours(src0) };
-	auto contours1{ getContours(src1) };
-	auto moment{ cv::moments(contours1.at(0)) };//模板的矩
-	cv::UMat huMoment;//模板的胡矩
-	cv::HuMoments(moment, huMoment);
-	int i{ 0 };
-	for (auto item : contours0) {
-		auto theMoment{ cv::moments(item) };
-		cv::UMat theHu;
-		cv::HuMoments(theMoment, theHu);
-		auto v{ cv::matchShapes(huMoment, theHu, cv::CONTOURS_MATCH_I1, 0) };
-		cout << "val" << ends << v << endl;
-		if (v < 1) {//即匹配高
-			cout << "the" << ends << i << endl;
-			cv::drawContours(src0, contours0, i, { 0,0,255 }, 2);
-		}
-		++i;
+	auto src{ cv::imread("E:/practice/openCV/material/images/stuff.jpg").getUMat(cv::ACCESS_FAST) };
+	cv::UMat canny;
+	cv::Canny(src, canny, 80, 160);
+	auto k{ cv::getStructuringElement(cv::MORPH_RECT,{3,3}) };
+	cv::UMat dilate;
+	cv::dilate(canny, dilate, k);
+
+	vector<cv::UMat>contours;
+	cv::findContours(dilate, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	for (auto item : contours) {
+		auto rotatedRect{ cv::fitEllipse(item) };
+		cv::ellipse(src, rotatedRect, { 0,0,255 }, 2);
 	}
-	cv::imshow("dst", src0);
+	cv::imshow("dst", src);
 
 	cv::waitKey(0);
 }
