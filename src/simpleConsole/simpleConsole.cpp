1732f8e2b3754ce7be73bd1db8640c16d5f06fa2
Zr <576172400@qq.com>
2020-09-05 23:02:51 +0800

    fitLine直线拟合

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,40 +1,53 @@
 ﻿#include<vector>
 #include<iostream>
 #include "opencv2/opencv.hpp"
-using std::vector;
 using std::cout;
 using std::endl;
 using std::ends;
+using std::vector;
 
 int main()
 {
-	auto src{ cv::imread("E:/practice/openCV/material/images/hand.jpg").getUMat(cv::ACCESS_FAST) };
-	cv::imshow("original", src);
-	//cv::UMat canny;
-	//cv::Canny(src, canny, 80, 160);
-	cv::UMat gray;
-	cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
-	cv::UMat bin;
-	cv::threshold(gray, bin, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-
-	auto k{ cv::getStructuringElement(cv::MORPH_RECT,{3,3}).getUMat(cv::ACCESS_FAST) };
-	cv::UMat morphy;
-	cv::morphologyEx(bin, morphy, cv::MORPH_OPEN, k);
-	//cv::imshow("binary",morphy);
-
-	vector<cv::UMat> contours;
-	cv::findContours(morphy, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-	for (const auto&item : contours) {//对于每一个轮廓
-		vector<cv::Point> ve;
-		cv::convexHull(item, ve);
-		if(!cv::isContourConvex(item)){//如果不是凸包轮廓则返回
-			//continue;//为啥每个都不是凸包??
+	auto src{ cv::imread("E:/openCV/opencv_tutorial/data/images/twolines.png") };
+	cv::Mat canny;
+	cv::Canny(src, canny, 80, 160);
+	auto k{ cv::getStructuringElement(cv::MORPH_RECT,{3,3}) };
+	//cv::Mat temp;
+	//cv::dilate(canny, temp, k);
+	//cv::imshow("dilate",temp);
+	vector<vector<cv::Point>>contours;
+	cv::findContours(canny, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	for (auto item : contours) {//每个轮廓
+		auto rect{ cv::boundingRect(item) };
+		auto v{ std::max(rect.width, rect.height) };//寻找最大的边
+		if (v < 30) {//如果最大的宽/高都小于30像素则不计
+			continue;
 		}
-		for (auto beg{ ve.cbegin() }; beg != ve.cend();++beg) {//凸包轮廓
-			cv::circle(src, *beg, 4, { 255,0,0}, 2);
-			auto nextPoint = beg == ve.cend() - 1 ? ve.cbegin() : beg + 1;//如果是最后一个点则连接第一个点
-			cv::line(src, *beg, *nextPoint, { 0,0,255 }, 2);
+		cv::Vec4f ve4;
+		cv::fitLine(item, ve4, cv::DIST_L1, 0, 0.01, 0.01);//拟合直线
+		//归一化后的vx,vy(单位1向量)
+		auto vx{ ve4[0] };
+		auto vy{ ve4[1] };
+		//线上的某个点
+		auto x0{ ve4[2] };
+		auto y0{ ve4[3] };
+		//斜率
+		auto k{ vy / vx };
+		if (std::abs(k) > 1e5) {//轮廓趋近于垂直时
+			continue;
 		}
+		//截距
+		auto b{ y0 - x0 * k };
+		int xMax{ INT_MIN }, xMin{ INT_MAX };
+		for (auto point:item) {//求得轮廓的x的最大最小值
+			xMax = xMax > point.x ? xMax : point.x;
+			xMin = xMin < point.x ? xMin : point.x;
+		}
+		//获得对应的y
+		int yMax = xMax * k + b;
+		int yMin = xMin * k + b;
+
+		cv::line(src, { xMin,yMin }, { xMax,yMax }, { 0,0,255 });
 	}
 	cv::imshow("dst", src);
 
