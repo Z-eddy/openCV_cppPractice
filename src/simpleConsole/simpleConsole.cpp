336847180a85d286e5c9648774edfffce121f911
赵睿 <zrendless@foxmail.com>
2020-09-08 17:18:35 +0800

    寻找内接圆太卡,改进算法,圈定ROI区域,仅在ROI区域内进行pointPolygonTest

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -17,24 +17,29 @@ int main()
 	cv::Mat dilate;
 	cv::dilate(canny, dilate, k);
 	vector<cv::Mat>contours;
-	cv::findContours(canny, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	cv::findContours(dilate, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 	cv::Mat dst{ src.size(),src.type(),{0,0,0} };
 	for (auto i{ 0 }; i != contours.size(); ++i) {
 		cv::drawContours(dst, contours, i, { 255,255,255 });//绘制本身的轮廓
+		auto rect{ cv::boundingRect(contours.at(i)) };
+		//dst(rect)=cv::Scalar{ 255,0,255 };
+		//cout << rect.x << ends << rect.y << endl;
 
-		cv::Mat data{ src.size(),CV_32F };//单通道用于计数
+		cv::Mat dataA{ src.size(),CV_32F };//单通道用于计数
+		auto data{ dataA(rect) };
 		auto row{ data.rows };
 		auto col{ data.cols };
 		for (int r{ 0 }; r != row; ++r) {//计算每个点距离轮廓的距离
 			auto p{ data.ptr<float>(r) };//每行的开头指针
 			for (int c{ 0 }; c != col; ++c) {
-				*p++ = cv::pointPolygonTest(contours.at(i), cv::Point{ c,r }, true);
+				*p++ = cv::pointPolygonTest(contours.at(i), cv::Point{ c + rect.x,r + rect.y }, true);
 				//data.at<float>(r,c) = cv::pointPolygonTest(contours.at(i), cv::Point{ c,r }, true);
 			}
 		}
 		double minV{ 0 }, maxV{ 0 };
 		cv::Point point;
-		cv::minMaxLoc(data, &minV, &maxV, nullptr, &point);
+		cv::minMaxLoc(dataA, &minV, &maxV, nullptr, &point);
+		//cout << minV << ends << maxV << '\t' << point.x << ends << point.y << endl;
 		if (maxV > 0) {
 			cv::circle(dst, point, maxV, { 0,0,255 });
 		}
