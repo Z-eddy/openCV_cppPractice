39131d7a0fddc9159bf95ee4dfe2d1251a8ec6e1
赵睿 <zrendless@foxmail.com>
2020-09-10 14:43:34 +0800

    HoughCircles霍夫圆检测

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -1,20 +1,24 @@
 ﻿#include<vector>
-#include<iostream>
-#include"opencv2/opencv.hpp"
+#include "opencv2/opencv.hpp"
 using std::vector;
-using std::cout;
-using std::endl;
-using std::ends;
 
 int main()
 {
-	auto src{ cv::imread("D:/temp.png").getUMat(cv::ACCESS_FAST) };
-	cv::UMat canny;
-	cv::Canny(src, canny, 80, 160);
-	vector<cv::Vec4i>ve;
-	cv::HoughLinesP(canny, ve, 1, CV_PI / 180, 80,120,10);//(src,ve,极坐标r步长,theta角度步长,统计点阈值)
-	for (const auto&item : ve) {//每条直线
-		cv::line(src, cv::Point(item[0],item[1]), cv::Point(item[2],item[3]), { 0,0,255 });
+	auto src{ cv::imread("E:/practice/openCV/material/images/coins.jpg").getUMat(cv::ACCESS_FAST) };
+	cv::UMat temp0;
+	cv::cvtColor(src, temp0, cv::COLOR_BGR2GRAY);
+	cv::UMat gaussian;
+	cv::GaussianBlur(temp0, gaussian, { 9,9 }, 2, 2);
+	vector<cv::Vec3f>ve;//储存霍夫圆的结果
+	/*
+	dp:检测内侧圆心的累加器图像的分辨率于输入图像之比的倒数，如dp=1，累加器和输入图像具有相同的分辨率，如果dp=2，累计器便有输入图像一半那么大的宽度和高度
+	*/
+	cv::HoughCircles(gaussian, ve, cv::HOUGH_GRADIENT, 2, 10, 100, 100, 20, 100);//(input,output,梯度法,dp,圆心之间的最小距离,梯度值阈值,累加值阈值,最小半径,最大半径)
+	for (const auto&item : ve) {
+		cv::Point center(item[0], item[1]);
+		auto r{ item[2] };
+		cv::circle(src, center, r, { 0,0,255 });
+		cv::circle(src, center, 3, { 255,0,255 },-1);
 	}
 	cv::imshow("dst", src);
 
