73b03e11a6bb0be703c230285f7b83a9bfaf7375
赵睿 <zrendless@foxmail.com>
2020-09-10 14:51:34 +0800

    霍夫圆检测,检测多个圆

--- src/simpleConsole/simpleConsole.cpp
+++ src/simpleConsole/simpleConsole.cpp
@@ -4,16 +4,18 @@ using std::vector;
 
 int main()
 {
-	auto src{ cv::imread("E:/practice/openCV/material/images/coins.jpg").getUMat(cv::ACCESS_FAST) };
+	//auto src{ cv::imread("E:/practice/openCV/material/images/coins.jpg").getUMat(cv::ACCESS_FAST) };
+	auto src{ cv::imread("E:/practice/openCV/material/images/FiGAUsSfZ6QlVLfJCFUFnFa1gNRN.png").getUMat(cv::ACCESS_FAST) };
+
 	cv::UMat temp0;
 	cv::cvtColor(src, temp0, cv::COLOR_BGR2GRAY);
 	cv::UMat gaussian;
-	cv::GaussianBlur(temp0, gaussian, { 9,9 }, 2, 2);
+	cv::GaussianBlur(temp0, gaussian, { 9,9 }, 1.5, 1.5);
 	vector<cv::Vec3f>ve;//储存霍夫圆的结果
 	/*
 	dp:检测内侧圆心的累加器图像的分辨率于输入图像之比的倒数，如dp=1，累加器和输入图像具有相同的分辨率，如果dp=2，累计器便有输入图像一半那么大的宽度和高度
 	*/
-	cv::HoughCircles(gaussian, ve, cv::HOUGH_GRADIENT, 2, 10, 100, 100, 20, 100);//(input,output,梯度法,dp,圆心之间的最小距离,梯度值阈值,累加值阈值,最小半径,最大半径)
+	cv::HoughCircles(gaussian, ve, cv::HOUGH_GRADIENT, 2, 10, 100, 80, 20, 100);//(input,output,梯度法,dp,圆心之间的最小距离,梯度值阈值,累加值阈值,最小半径,最大半径)
 	for (const auto&item : ve) {
 		cv::Point center(item[0], item[1]);
 		auto r{ item[2] };
